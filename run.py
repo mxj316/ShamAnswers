bcf1ec39a82e0fbe3db9cfdc56b50e615dca320e
David Meshnick <dcmesh@Davids-MacBook-Pro.local>
2019-04-27 19:16:41 -0400

    Preliminary files

--- run.py
+++ run.py
@@ -1,35 +1,71 @@
-from flask import Flask, render_template
-import datetime
+import configparser
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+import mysql.connector
+import datetime as datetime
+from User import User
 
+# Read configuration from file.
+config = configparser.ConfigParser()
+config.read('config.ini')
+my_user = None  # will hold user id whene logged in
+# Set up application server.
 app = Flask(__name__)
+app.secret_key = 'my_key_is_set_here'
 
-@app.template_filter()
-def datetimefilter(value, format='%Y/%m/%d %H:%M'):
-    """convert a datetime to a different format."""
-    return value.strftime(format)
 
-app.jinja_env.filters['datetimefilter'] = datetimefilter
+# Create a function for fetching data from the database.
+def sql_query(sql):
+    db = mysql.connector.connect(**config['mysql.connector'])
+    cursor = db.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return result
 
-@app.route("/")
-def template_test():
-    return render_template('template.html', my_string="Wheeeee!", 
-        my_list=[0,1,2,3,4,5], title="Index", current_time=datetime.datetime.now())
 
-@app.route("/home")
-def home():
-    return render_template('template.html', my_string="Foo", 
-        my_list=[6,7,8,9,10,11], title="Home", current_time=datetime.datetime.now())
+def sql_execute(sql):
+    db = mysql.connector.connect(**config['mysql.connector'])
+    cursor = db.cursor()
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
 
-@app.route("/about")
-def about():
-    return render_template('template.html', my_string="Bar", 
-        my_list=[12,13,14,15,16,17], title="About", current_time=datetime.datetime.now())
 
-@app.route("/contact")
-def contact():
-    return render_template('template.html', my_string="FooBar"
-        , my_list=[18,19,20,21,22,23], title="Contact Us", current_time=datetime.datetime.now())
+# For this example you can select a handler function by
+# uncommenting one of the @app.route decorators.
+
+@app.route('/')
+def basic_response():
+    return redirect(url_for('start'))
+
+@app.route('/start', methods=['GET', 'POST'])
+def start()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login()
+
+@app.route('/createaccount', methods=['GET', 'POST'])
+def createaccount()
+
+@app.route('/main', methods=['GET', 'POST'])
+def main()
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile()
+
+@app.route('/updateaccount', methods=['GET', 'POST'])
+def update_account()
+
+@app.route('/deleteaccount', methods=['GET', 'POST'])
+def delete_account()
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout()
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(**config['app'])
+{}
