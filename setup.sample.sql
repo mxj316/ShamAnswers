ddf47019a79abdaa4baa78467f1a2688b4ec2a2b
Shaan Patel <sbp45@case.edu>
2019-05-02 04:16:49 -0400

    Sample data

--- setup.sample.sql
+++ setup.sample.sql
@@ -52,3 +52,20 @@ create table vote(
     foreign key (user_id) references user(id),
     foreign key (letter_id) references letter(id)
 );
+
+-- insert data into database (temporary data for testing if needed)
+insert into user(username, email, password) values('Coolsnail123', 'raj@case.edu', 'Skyrim');
+insert into user(username, email, password) values('SgtPepper', 'pepperkep@case.edu', 'Inside');
+insert into user(username, email, password) values('Meshy00', 'organizer@case.edu', 'DivinityTho');
+insert into user(username, email, password) values('Andre3000', 'micycle@case.edu', 'Motorcycle');
+insert into user(username, email, password) values('JosephJoeStarII', 'noobmaster69@case.edu', 'MemeLord');
+
+insert into question(content, category, user_id) values('Are Javadoc comments the coolest thing ever?', 'Advice', 1);
+insert into question(content, category, user_id) values('Is Hot Pie the key to everything?', 'Humor', 4);
+
+insert into letter(alphabet_letter, user_id, question_id, sub_letter_id) values('Y', 2, 2, null);
+insert into letter(alphabet_letter, user_id, question_id, sub_letter_id) values('E', 2, 2, 1);
+insert into letter(alphabet_letter, user_id, question_id, sub_letter_id) values('T', 2, 2, 1);
+insert into letter(alphabet_letter, user_id, question_id, sub_letter_id) values('E', 4, 1, 2);
+insert into letter(alphabet_letter, user_id, question_id, sub_letter_id) values('O', 1, 2, 3);
+insert into letter(alphabet_letter, user_id, question_id, sub_letter_id) values('K', 2, 2, 4);
